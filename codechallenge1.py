88486e6ea80efeb50bd4730fedc3db221509fd01
shairajoynm <shairacalapit462@gmail.com>
2025-08-12 19:47:32 +0800

    Initial commit of codechallenge1.py

---
+++ codechallenge1.py
@@ -0,0 +1,7 @@
+name = input("Type your name")
+
+print("\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t*\t\t\t\t*\n\n \t*\t\t\t Hi\t\t\t* \n\n *\t\t\t ",name,"\t\t\t\t* \n\n \t*\t\t\t\t\t\t* \n\n \t\t*\t\t\t\t* \n\n \t\t\t*\t\t* \n\n \t\t\t\t*")
+
+
+
+
