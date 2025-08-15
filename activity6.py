8b521fd6a6261d8dcc0269d4a7f0fc8024546517
shairajoynm <shairacalapit462@gmail.com>
2025-08-15 16:37:31 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,17 @@
+x = eval(input("Enter the first number here -> "))
+
+y = eval(input("Enter the second number here -> "))
+
+p = x + y
+mi = x - y
+mu = x * y
+div = x - y
+
+print("Sum of", x, "and", y, "is",p)
+print("Difference of", x, "and", y, "is",mi)
+print("Product of", x, "and", y, "is", mu)
+print("Quotient of", x, "and", y, "is", div)
+
+answer = (p + mi + mu + div)
+
+print("\nThe summation of the all the operation is", answer)
