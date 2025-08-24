eef2cdf0c080c22139c670eb8521a207a2284c2d
shairajoynm <shairacalapit462@gmail.com>
2025-08-24 17:27:58 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,20 @@
+email = 'adenosinetriphosphate@smail.com'
+password = 'ayosnaman'
+
+
+#or
+print("In 'or' operator")
+print((email == 'adenosinetriphosphate@smail.com') or (password == 'ayosnaman'))
+print(not(email == 'adenosinetriphosphate@smail.com') or (password == 'ayosnaman'))
+print(email == 'adenosinetriphosphate@smail.com') or (not(password == 'ayosnaman'))
+print(not(email == 'adenosinetriphosphate@smail.com')) or (not(password == 'ayosnaman'))
+
+
+
+#and
+print("\nIn 'and' operator")
+print((email == 'adenosinetriphosphate@smail.com') and (password == 'ayosnaman'))
+print(not(email == 'adenosinetriphosphate@smail.com') and (password == 'ayosnaman'))
+print(email == 'adenosinetriphosphate@smail.com') and (not(password == 'ayosnaman'))
+print(not(email == 'adenosinetriphosphate@smail.com')) and (not(password == 'ayosnaman'))
+
