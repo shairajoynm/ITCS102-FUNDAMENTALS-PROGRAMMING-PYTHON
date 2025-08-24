90eeb82d8575bb0fa40d53f753c965306ee69096
shairajoynm <shairacalapit462@gmail.com>
2025-08-24 16:15:10 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,15 @@
+
+number = int(input("Enter any number -->  "))
+
+print("a variable has a value of", number)
+
+number = number + 1
+
+print("the system first added the variable to 1 then add, subtract, multiply and divide the", number, "to the integers 2,3,4 and 5")
+
+number += 2
+number -= 3
+number *= 4
+number /= 5
+
+print("the final value of the variable will be", number)
