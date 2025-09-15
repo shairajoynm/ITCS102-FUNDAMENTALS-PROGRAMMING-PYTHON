ee9cc738ebeb0640948668752c4eca8ed1866ac8
shairajoynm <shairacalapit462@gmail.com>
2025-09-15 14:55:10 +0800

    Add files via upload

---
+++ codechallenge3.py
@@ -0,0 +1,10 @@
+username = "rubyshai"
+passz = "howyoulikethat"
+
+un = input("Username: ")
+password = input("Password: ")
+
+if username == un and passz == password:
+	print("Access Granted")
+else:
+	print("Access Denied. Please try again")
