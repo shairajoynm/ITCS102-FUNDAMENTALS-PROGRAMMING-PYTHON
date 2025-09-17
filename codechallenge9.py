beef9763d554a5339a3c826d5a2eededf8e3247c
shairajoynm <shairacalapit462@gmail.com>
2025-09-17 20:02:44 +0800

    Add files via upload

---
+++ codechallenge9.py
@@ -0,0 +1,8 @@
+#Countdown timer simulator
+print("Welcome to Countdown Timer Simulator!\n")
+count_d = eval(input("\tStart: \n\n"))
+print("Countdown started: ")
+
+for start in range(count_d, 0, -1):
+      print(" ", start)
+print("Liftoff!")
