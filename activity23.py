9049729bf50862ec3ff5a5d9f45a663b5d8d7371
shairajoynm <shairacalapit462@gmail.com>
2025-11-07 10:10:07 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,25 @@
+# months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+# months.append('Jan')
+# print(months)
+# months.pop()
+# print(months)
+
+
+# name = 'Shaira Joy'
+# for s in name:
+#     print(s)
+
+# s = ['Shaira', 'Joy', 'Calapit']
+
+# print('Firstname is', s[0])
+
+name = 'Lalisa Manoban'
+for access in name[5 : : -1]:
+    print(access)
+
+name1 = 'Lalisa Manoban'
+e = (list(name1))
+print(e)
+e.reverse()
+print(e)
+print(e[3])
